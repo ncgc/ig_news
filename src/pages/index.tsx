0f5efb937136c5449b86d9802f703d5907ebfdac
Natalia <natalia.datascience@gmail.com>
2022-02-24 09:52:23 -0300

    Change to Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../../styles/home.module.scss'
 import { SubscribeButton } from '../components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { stripe } from '../services/stripe'
 
 interface HomeProps {
@@ -33,7 +33,7 @@ export default function Home( {product}:HomeProps ) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KW2T9FKOiu69lxRgekUbsww',{
     expand: ['product'] //permite pegar todos os dados do produto
   }) 
@@ -49,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 horas
   }
 }
