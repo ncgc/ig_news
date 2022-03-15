c54118ae05df63603d60033071fa20a42bdbb595
Natalia <natalia.datascience@gmail.com>
2022-03-15 01:08:27 -0300

    Add other events listening

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { TRUE } from "sass";
 import { Readable } from 'stream'
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
@@ -21,7 +22,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  // 'customer.subscription.created',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
 ])
 
 export default async(req: NextApiRequest, res: NextApiResponse)=> {
@@ -41,11 +45,22 @@ export default async(req: NextApiRequest, res: NextApiResponse)=> {
     if (relevantEvents.has(type)){
       try{
       switch(type){
+        case 'customer.subscription.updated':
+        case 'customer.subscription.deleted':
+          const subscription = event.data.object as Stripe.Subscription;
+
+          await saveSubscription(
+            subscription.id,
+            subscription.customer.toString(),
+            false
+          );
+          break;
         case 'checkout.session.completed':
           const checkoutSession = event.data.object as Stripe.Checkout.Session
           await saveSubscription(
             checkoutSession.subscription.toString(),
-            checkoutSession.customer.toString()
+            checkoutSession.customer.toString(),
+            true
           )
           break;
         default:
