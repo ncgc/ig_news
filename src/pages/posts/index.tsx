bddaeb970ab0bdf2285308edeb9beed8a7731f85
Natalia <natalia.datascience@gmail.com>
2022-05-29 10:57:15 -0300

    Add post content page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import * as prismic from '@prismicio/client'
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 import { RichText } from 'prismic-dom'
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -25,11 +26,13 @@ export default function Posts({ posts }: PostsProps){
     <main className={styles.container}>
       <div className={styles.posts}>
         { posts.map(post => (
-          <a key={post.slug} href="#">
-            <time>{post.updatedAt}</time>
-            <strong>{post.title}</strong>
-            <p>{post.excerpt}</p>
-          </a>
+          <Link href={`/posts/${post.slug}`}>
+            <a key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          </Link>
         ))}
       </div>
     </main>
@@ -55,8 +58,6 @@ export const getStaticProps: GetStaticProps = async () => {
       })
     }
   })
-
-  console.log(posts);
 
   return {
     props: {
